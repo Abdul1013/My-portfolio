5048bd591f60680cac582c68ae9b115ef4e40ff3
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2024-07-05 19:47:28 +0100

    edit

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ function updateDateTime() {
 
     updateDay(); 
 });
+// Hamburger menu toggle
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
